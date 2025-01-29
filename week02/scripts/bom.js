798fd4bcfb8f1d67ff5c9e5ef7d195ceda318496
melitonmedina <melitonmedina@byui.edu>
2025-01-28 22:31:58 -0500

    week02-bomApp

---
+++ week02/scripts/bom.js
@@ -0,0 +1,38 @@
+// variables to hold references to the input, button, & list
+const input = document.querySelector(`#favchap`);
+const button = document.querySelector(`button`);
+const list = document.querySelector(`#list`);
+
+//creating an event listener for the submit button
+button.addEventListener('click', function(){
+    if (input.value.trim() !==''){
+        console.log(`You forgot to enter your scripture`);
+        // the li elements
+        const li= document.createElement('li');
+        li.textContent = input.value;
+
+        //the delete button
+        const deleteButton = document.createElement(`button`);        
+        deleteButton.textContent = `❌`;
+        deleteButton.className = 'deleteButton';
+
+        //appending the list
+        li.append(deleteButton);
+        list.append(li);
+        list.appendChild(li);
+
+    }
+    else (input.focus());
+
+}
+)
+
+//let's handle the delete button accessing the list
+
+list.addEventListener('click', function(event){
+    if (event.target.classList.contains('deleteButton')) {
+        event.target.parentNode.remove();
+        input.focus();
+        input.value = '';
+    }
+})
