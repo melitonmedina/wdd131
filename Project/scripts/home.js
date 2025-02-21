325598efa913403935d96b1616872db2fa15e995
melitonmedina <melitonmedina@byui.edu>
2025-02-21 17:13:18 -0500

    project-part3-v1

--- Project/scripts/home.js
+++ Project/scripts/home.js
@@ -221,13 +221,3 @@ document.addEventListener('DOMContentLoaded', () => {
     
   });
 
-  // subcriber accumulator
-  // review counter increment
-document.addEventListener('DOMContentLoaded', function(){
-    let counterSubs = localStorage.getItem('subcriberCounter') || 0;
-    counterSubs = parseInt(counterSubs, 10);
-    counterSubs++;
-
-    localStorage.setItem('subcriberCounter', counterSubs);
-    document.getElementById('counterSubs').textContent = `Total Subscribers: ${counterSubs}`;
-})
