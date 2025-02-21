36acab1d5c39f0de3a1603d13ab61a321696a3fa
melitonmedina <melitonmedina@byui.edu>
2025-02-21 17:32:32 -0500

    project-images-fixed

--- Project/scripts/home.js
+++ Project/scripts/home.js
@@ -81,7 +81,7 @@ const upcoming = [
         description: "Become a Master Ecologist while using Duke Farms as a living laboratory and network with field experts and professionals.",
         date: "THU, FEB 20",
         time: "7:00 PM",
-        imageUrl: "/Project/images/webp/nature-2943774_640.webp"
+        imageUrl: "../Project/images/webp/nature-2943774_640.webp"
 
     },
     {
@@ -89,7 +89,7 @@ const upcoming = [
         description: "BAn introduction to eagle-eyed birding! Our nest cam is back online for the season! ",
         date: "SAT, MAR 1",
         time: "9:30 AM",
-        imageUrl: "/Project/images/webp/pompadour-3906740_640.webp"
+        imageUrl: "../Project/images/webp/pompadour-3906740_640.webp"
 
     },
     {
@@ -97,7 +97,7 @@ const upcoming = [
         description: "Join us for a special program that connects the past, present, and future of ecological restoration along the Raritan River.",
         date: "SAT, MAR 8",
         time: "10:00 AM",
-        imageUrl: "/Project/images/webp/raptor-2008906_640.webp"
+        imageUrl: "../Project/images/webp/raptor-2008906_640.webp"
 
     },
     {
@@ -105,7 +105,7 @@ const upcoming = [
         description: "Winter can seem like a time to put things on hold, for both the natural world and those of us who study and enjoy it",
         date: "SAT, MAR 8",
         time: "2:00 PM",
-        imageUrl: "/Project/images/webp/winter-4742436_640.webp"
+        imageUrl: "../Project/images/webp/winter-4742436_640.webp"
     },
 
     {
@@ -113,7 +113,7 @@ const upcoming = [
         description: "A bracing winter night’s walk, illuminated by March’s full moon, transports us to the Orchid Range, and from there: Panama? Nepal? Ghana?",
         date: "FRI, MAR 14",
         time: "2:00 PM",
-        imageUrl: "/Project/images/webp/greenhouse-4262241_640.webp"
+        imageUrl: "../Project/images/webp/greenhouse-4262241_640.webp"
     },
 ]
 
